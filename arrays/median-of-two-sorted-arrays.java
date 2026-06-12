7460cc174f6517df1a70c628d92f16b0a8cab9ea
sunainaballary957-cmyk <sunainaballary957@gmail.com>
2026-06-12 23:17:40 +0530

    Create median-of-two-sorted-arrays.java
    solved #4 median-of-two-sorted-arrays

---
+++ arrays/median-of-two-sorted-arrays.java
@@ -0,0 +1,35 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int[] merged = new int[m + n];
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                merged[k++] = nums1[i++];
+            } else {
+                merged[k++] = nums2[j++];
+            }
+        }
+
+        while (i < m) {
+            merged[k++] = nums1[i++];
+        }
+
+        while (j < n) {
+            merged[k++] = nums2[j++];
+        }
+
+        int len = m + n;
+
+        if (len % 2 == 1) {
+            return merged[len / 2];
+        } else {
+            return (merged[len / 2 - 1] + merged[len / 2]) / 2.0;
+        }
+    }
+}
