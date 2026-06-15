750989b44a031ba69e81afca3c172d0d1c0fc66f
sunainaballary957-cmyk <sunainaballary957@gmail.com>
2026-06-15 11:27:19 +0530

    Create letter-combinations-of-a-phone-number.java
    solved leetcode #17 letter-combinations-of-a-phone-number

---
+++ arrays/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+
+    List<String> result = new ArrayList<>();
+
+    String[] phone = {
+        "", "", "abc", "def",
+        "ghi", "jkl", "mno",
+        "pqrs", "tuv", "wxyz"
+    };
+
+    public List<String> letterCombinations(String digits) {
+
+        if (digits == null || digits.length() == 0) {
+            return result;
+        }
+
+        backtrack(digits, 0, new StringBuilder());
+
+        return result;
+    }
+
+    private void backtrack(String digits,
+                           int index,
+                           StringBuilder current) {
+
+        if (index == digits.length()) {
+            result.add(current.toString());
+            return;
+        }
+
+        String letters =
+            phone[digits.charAt(index) - '0'];
+
+        for (char c : letters.toCharArray()) {
+
+            current.append(c);
+
+            backtrack(digits,
+                      index + 1,
+                      current);
+
+            current.deleteCharAt(
+                current.length() - 1
+            );
+        }
+    }
+}
